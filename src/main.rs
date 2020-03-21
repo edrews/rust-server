ce4b19c0635eba4cc8c05f67023c68bf4451cebe
Eric Drews <edrews@gmail.com>
2020-03-21 00:07:17 -0400

    Remove unwraps

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
+use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
+    match run_server() {
+        Ok(()) => {
+            println!("Stopping server");
+        }
+        Err(_e) => {
+            println!("Error running server");
+        }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn run_server() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream)?;
+            }
+            Err(_e) => {
+                println!("Connection failed");
+            }
+        }
+    }
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    let contents = fs::read_to_string("html/hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.read(&mut buffer)?;
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("html/hello.html")?;
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes())?;
+    } else {
+        let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        stream.write(response.as_bytes())?;
+    }
+    stream.flush()?;
+    Ok(())
 }
